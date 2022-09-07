92b4a4df0c96dbf864f4736c978bf92e64262cd1
Dominika Gzela <dominikagzela@gmail.com>
2022-09-07 22:15:02 +0200

    models

---
+++ personal_trainer/management_app/models.py
@@ -0,0 +1,64 @@
+from django.db import models
+from django.contrib.auth.models import AbstractUser
+
+TRAINING = (
+    (1, 'Trening pierwszy'),
+    (2, 'Trening drugi'),
+    (3, 'Trening trzeci'),
+    (4, 'Trening czwarty'),
+)
+
+class User(AbstractUser):
+    is_trainer = models.BooleanField(default=False)
+    name = models.CharField(max_length=255)
+    surname = models.CharField(max_length=255)
+    purpose = models.TextField()
+    # email = models.EmailField()
+    # password = models.CharField()
+
+
+class MacroElements(models.Model):
+    calories = models.IntegerField()
+    protein = models.IntegerField()
+    fat = models.IntegerField()
+    carb = models.IntegerField()
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+
+
+class Reports(models.Model):
+    weight = models.DecimalField(max_digits=4, decimal_places=1)
+    waist = models.DecimalField(max_digits=4, decimal_places=1)
+    stomach = models.DecimalField(max_digits=4, decimal_places=1)
+    hip = models.DecimalField(max_digits=4, decimal_places=1)
+    thigh = models.DecimalField(max_digits=4, decimal_places=1)
+    training_first = models.BooleanField()
+    training_second = models.BooleanField()
+    training_third = models.BooleanField()
+    training_fourth = models.BooleanField()
+    comments = models.TextField()
+    created_date = models.DateField(auto_created=True)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+
+
+class Photos(models.Model):
+    front = models.ImageField(upload_to='photos')
+    back = models.ImageField(upload_to='photos')
+    right = models.ImageField(upload_to='photos')
+    left = models.ImageField(upload_to='photos')
+    report = models.ForeignKey(Reports, on_delete=models.CASCADE)
+
+
+class Exercises(models.Model):
+    name = models.CharField(max_length=255)
+    description = models.TextField()
+    url = models.URLField()
+    plan = models.ManyToManyField(User, through='PlanExercises')
+
+
+class PlanExercises(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    exercise = models.ForeignKey(Exercises, on_delete=models.CASCADE)
+    training_number = models.IntegerField(TRAINING)
+    series = models.IntegerField()
+    repeat = models.IntegerField(null=True)
+    TUT = models.CharField(max_length=7)
