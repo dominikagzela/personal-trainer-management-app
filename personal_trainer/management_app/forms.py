9ad8ba6cd30bbcdc218977f7e164edda67ba4f92
dominikagzela <dominikagzela@gmail.com>
2023-06-02 18:03:30 +0200

    typing_data

--- personal_trainer/management_app/forms.py
+++ personal_trainer/management_app/forms.py
@@ -13,8 +13,8 @@ class LoginUserForm(forms.Form):
     """
     Login form.
     """
-    username = forms.CharField(label='Login', max_length=265)
-    password = forms.CharField(label='Hasło', widget=forms.PasswordInput)
+    username: str = forms.CharField(label='Login', max_length=265)
+    password: str = forms.CharField(label='Hasło', widget=forms.PasswordInput)
 
 
 class PracticalTipForm(forms.ModelForm):
@@ -31,11 +31,11 @@ class MacroElementsForm(forms.ModelForm):
     """
     Macro elements form for superuser.
     """
-    STEP_SIZE = 1
-    calories = forms.IntegerField(label='Kalorie (kcal)', min_value=1000, max_value=8000, step_size=STEP_SIZE)
-    protein = forms.IntegerField(label='Białko (g)', min_value=1, max_value=800, step_size=STEP_SIZE)
-    fat = forms.IntegerField(label='Tłuszcze (g)', min_value=1, max_value=800, step_size=STEP_SIZE)
-    carb = forms.IntegerField(label='Węglowodany (g)', min_value=1, max_value=1500, step_size=STEP_SIZE)
+    STEP_SIZE: int = 1
+    calories: int = forms.IntegerField(label='Kalorie (kcal)', min_value=1000, max_value=8000, step_size=STEP_SIZE)
+    protein: int = forms.IntegerField(label='Białko (g)', min_value=1, max_value=800, step_size=STEP_SIZE)
+    fat: int = forms.IntegerField(label='Tłuszcze (g)', min_value=1, max_value=800, step_size=STEP_SIZE)
+    carb: int = forms.IntegerField(label='Węglowodany (g)', min_value=1, max_value=1500, step_size=STEP_SIZE)
 
     class Meta:
         model = MacroElements
@@ -64,12 +64,12 @@ class PlanExercisesForm(forms.ModelForm):
     """
     Form for plan of exercises for superuser.
     """
-    STEP_SIZE = 1
-    MIN_VALUE = 1
+    STEP_SIZE: int = 1
+    MIN_VALUE: int = 1
     exercise = forms.ModelChoiceField(queryset=Exercises.objects.all(), label='Ćwiczenie',
                                       empty_label=None, to_field_name='name')
-    series = forms.IntegerField(label='Serie', min_value=MIN_VALUE, max_value=10, step_size=STEP_SIZE)
-    repeat = forms.IntegerField(label='Powtórzenia', min_value=MIN_VALUE, max_value=50, step_size=STEP_SIZE)
+    series: int = forms.IntegerField(label='Serie', min_value=MIN_VALUE, max_value=10, step_size=STEP_SIZE)
+    repeat: int = forms.IntegerField(label='Powtórzenia', min_value=MIN_VALUE, max_value=50, step_size=STEP_SIZE)
 
     class Meta:
         model = PlanExercises
@@ -85,19 +85,20 @@ class ReportForm(forms.ModelForm):
     """
     Report form for client.
     """
-    MIN_VALUE = 20
-    MAX_VALUE = 300
-    STEP_SIZE = 1
-    weight = forms.IntegerField(label='Waga (kg)', min_value=MIN_VALUE, max_value=MAX_VALUE, step_size=STEP_SIZE)
-    waist = forms.IntegerField(label='Talia (cm)', min_value=MIN_VALUE, max_value=MAX_VALUE, step_size=STEP_SIZE)
-    stomach = forms.IntegerField(label='Brzuch (cm)', min_value=MIN_VALUE, max_value=MAX_VALUE, step_size=STEP_SIZE)
-    hip = forms.IntegerField(label='Biodra (cm)', min_value=MIN_VALUE, max_value=MAX_VALUE, step_size=STEP_SIZE)
-    thigh = forms.IntegerField(label='Udo (cm)', min_value=MIN_VALUE, max_value=MAX_VALUE, step_size=STEP_SIZE)
-    training_first = forms.BooleanField(label='Trening 1', required=False)
-    training_second = forms.BooleanField(label='Trening 2', required=False)
-    training_third = forms.BooleanField(label='Trening 3', required=False)
-    training_fourth = forms.BooleanField(label='Trening 4', required=False)
-    comments = forms.CharField(widget=forms.Textarea, label='Komentarz')
+    MIN_VALUE: int = 20
+    MAX_VALUE: int = 300
+    STEP_SIZE: int = 1
+
+    weight: int = forms.IntegerField(label='Waga (kg)', min_value=MIN_VALUE, max_value=MAX_VALUE, step_size=STEP_SIZE)
+    waist: int = forms.IntegerField(label='Talia (cm)', min_value=MIN_VALUE, max_value=MAX_VALUE, step_size=STEP_SIZE)
+    stomach: int = forms.IntegerField(label='Brzuch (cm)', min_value=MIN_VALUE, max_value=MAX_VALUE, step_size=STEP_SIZE)
+    hip: int = forms.IntegerField(label='Biodra (cm)', min_value=MIN_VALUE, max_value=MAX_VALUE, step_size=STEP_SIZE)
+    thigh: int = forms.IntegerField(label='Udo (cm)', min_value=MIN_VALUE, max_value=MAX_VALUE, step_size=STEP_SIZE)
+    training_first: bool = forms.BooleanField(label='Trening 1', required=False)
+    training_second: bool = forms.BooleanField(label='Trening 2', required=False)
+    training_third: bool = forms.BooleanField(label='Trening 3', required=False)
+    training_fourth: bool = forms.BooleanField(label='Trening 4', required=False)
+    comments: str = forms.CharField(widget=forms.Textarea, label='Komentarz')
 
     class Meta:
         model = Reports
@@ -108,6 +109,7 @@ class PhotosForm(forms.ModelForm):
     """
     Photos form for creating report by client.
     """
+
     class Meta:
         model = Photos
         exclude = ['report']
@@ -123,7 +125,7 @@ class ReportPhotosMultiForm(MultiModelForm):
     """
     Report and photos form combined.
     """
-    form_classes = {
+    form_classes: dict = {
         'report': ReportForm,
         'photos': PhotosForm,
     }
--- personal_trainer/management_app/models.py
+++ personal_trainer/management_app/models.py
@@ -13,39 +13,39 @@ class User(AbstractUser):
     """
     User model.
     """
-    is_trainer = models.BooleanField(default=False)
-    purpose = models.TextField()
+    is_trainer: bool = models.BooleanField(default=False)
+    purpose: str = models.TextField()
 
 
 class MacroElements(models.Model):
     """
     Macro elements model for user.
     """
-    calories = models.IntegerField()
-    protein = models.IntegerField()
-    fat = models.IntegerField()
-    carb = models.IntegerField()
-    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    calories: int = models.IntegerField()
+    protein: int = models.IntegerField()
+    fat: int = models.IntegerField()
+    carb: int = models.IntegerField()
+    user: User = models.OneToOneField(User, on_delete=models.CASCADE)
 
 
 class Reports(models.Model):
     """
     Report model for creating weekly report with measurements and trainings completed by user.
     """
-    MAX_DIGITS = 4
-    DECIMAL_PLACES = 4
-    weight = models.DecimalField(max_digits=MAX_DIGITS, decimal_places=DECIMAL_PLACES)
-    waist = models.DecimalField(max_digits=MAX_DIGITS, decimal_places=DECIMAL_PLACES)
-    stomach = models.DecimalField(max_digits=MAX_DIGITS, decimal_places=DECIMAL_PLACES)
-    hip = models.DecimalField(max_digits=MAX_DIGITS, decimal_places=DECIMAL_PLACES)
-    thigh = models.DecimalField(max_digits=MAX_DIGITS, decimal_places=DECIMAL_PLACES)
-    training_first = models.BooleanField()
-    training_second = models.BooleanField()
-    training_third = models.BooleanField()
-    training_fourth = models.BooleanField()
-    comments = models.TextField()
-    created_date = models.DateField(auto_now_add=True)
-    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    MAX_DIGITS: int = 4
+    DECIMAL_PLACES: int = 4
+    weight: int = models.DecimalField(max_digits=MAX_DIGITS, decimal_places=DECIMAL_PLACES)
+    waist: int = models.DecimalField(max_digits=MAX_DIGITS, decimal_places=DECIMAL_PLACES)
+    stomach: int = models.DecimalField(max_digits=MAX_DIGITS, decimal_places=DECIMAL_PLACES)
+    hip: int = models.DecimalField(max_digits=MAX_DIGITS, decimal_places=DECIMAL_PLACES)
+    thigh: int = models.DecimalField(max_digits=MAX_DIGITS, decimal_places=DECIMAL_PLACES)
+    training_first: bool = models.BooleanField()
+    training_second: bool = models.BooleanField()
+    training_third: bool = models.BooleanField()
+    training_fourth: bool = models.BooleanField()
+    comments: str = models.TextField()
+    created_date= models.DateField(auto_now_add=True)
+    user: User = models.ForeignKey(User, on_delete=models.CASCADE)
 
     class Meta:
         ordering = ['created_date']
@@ -55,7 +55,7 @@ class Photos(models.Model):
     """
     Photos model for creating weekly report by user.
     """
-    UPLOAD_FILE = 'photos'
+    UPLOAD_FILE: str = 'photos'
     front = models.ImageField(upload_to=UPLOAD_FILE)
     back = models.ImageField(upload_to=UPLOAD_FILE)
     right = models.ImageField(upload_to=UPLOAD_FILE)
@@ -67,9 +67,9 @@ class Exercises(models.Model):
     """
     Exercises model for superuser.
     """
-    name = models.CharField(max_length=255)
-    description = models.TextField()
-    url = models.URLField()
+    name: str = models.CharField(max_length=255)
+    description: str = models.TextField()
+    url: str = models.URLField()
     plan = models.ManyToManyField(User, through='PlanExercises')
 
     def __str__(self):
@@ -83,12 +83,12 @@ class PlanExercises(models.Model):
     """
     Model of exercises included in users plan.
     """
-    user = models.ForeignKey(User, on_delete=models.CASCADE)
-    exercise = models.ForeignKey(Exercises, on_delete=models.CASCADE)
-    training_number = models.IntegerField(TRAINING)
-    series = models.IntegerField()
-    repeat = models.IntegerField(null=True)
-    TUT = models.CharField(max_length=7, null=True)
+    user: User = models.ForeignKey(User, on_delete=models.CASCADE)
+    exercise: Exercises = models.ForeignKey(Exercises, on_delete=models.CASCADE)
+    training_number: int = models.IntegerField(TRAINING)
+    series: int = models.IntegerField()
+    repeat: int = models.IntegerField(null=True)
+    TUT: str = models.CharField(max_length=7, null=True)
 
     class Meta:
         ordering = ['training_number', 'pk']
@@ -98,7 +98,7 @@ class PracticalTips(models.Model):
     """
     Practical tips model.
     """
-    tip = models.TextField()
+    tip: str = models.TextField()
 
     class Meta:
         ordering = ['pk']
